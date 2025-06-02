220327a1a695014f98105527e5e587f9b714774d
Samuel Legrain <samuel.legrain@bstorm.be>
2025-06-02 11:52:49 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-Namespace/Program.cs
@@ -0,0 +1,15 @@
+﻿using Demo_Namespace.Inventaire;
+
+namespace Demo_Namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item objetInventaire = new Item();
+            objetInventaire.nom = "Pépite";
+            objetInventaire.value = 5000;
+            Console.WriteLine($"Dans mon sac, j'ai une {objetInventaire.nom} de {objetInventaire.value} Pokédollars !");
+        }
+    }
+}
