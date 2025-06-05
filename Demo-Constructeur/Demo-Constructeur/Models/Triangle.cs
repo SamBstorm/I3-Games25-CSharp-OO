8a4c7181e9ccd5d46b7cb4316a100e4694a5c218
michael person <michael.person@cognitic.be>
2025-06-05 15:41:49 +0200

    Ajout Demo Constructeur

---
+++ Demo-Constructeur/Demo-Constructeur/Models/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Constructeur.Models
+{
+    internal class Triangle
+    {
+        private int _cotea;
+        private int _coteb;
+        private int _cotec;
+
+
+        public Triangle(int cote1=5, int cote2=6, int cote3=15)
+        {
+                _cotea = cote1;
+            _coteb = cote2;
+            _cotec = cote3;
+        }
+    }
+}
---
+++ Demo-Constructeur/Demo-Constructeur/Program.cs
@@ -0,0 +1,15 @@
+﻿using Demo_Constructeur.Models;
+
+namespace Demo_Constructeur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Personne deuxieme = new Personne("Alba","Jessica"); 
+            Console.WriteLine( deuxieme.Nom);
+        }
+    }
+}
