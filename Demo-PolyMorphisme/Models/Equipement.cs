a5f7d5a28cb7414ce11c33db27e216635bfbf95f
Samuel Legrain <samuel.legrain@bstorm.be>
2025-06-16 12:02:30 +0200

    Ajout Démo Polymorphisme

---
+++ Demo-PolyMorphisme/Models/Equipement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_PolyMorphisme.Models
+{
+    internal class Equipement : InventaireItem
+    {
+        public int BonusDef { get; private set; }
+        public int BonusAtk { get; private set; }
+        public Equipement(string nom, int prix, int bonusDef, int bonusAtk) : base(nom, prix)
+        {
+            BonusDef = bonusDef;
+            BonusAtk = bonusAtk;
+        }
+    }
+}
